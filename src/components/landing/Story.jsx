f1f96f98f9998b2b0854863565fb180b59eba3a2
kemalmao19 <sebelumtidur14@gmail.com>
2023-12-31 01:44:38 +0700

    feat: landing page & my pets

---
+++ src/components/landing/Story.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Story = ({ stories }) => {
+  return (
+    <>
+          <h2 className="my-5">
+        <span className="text-oren">Adopter</span> Stories
+      </h2>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 my-20">
+      </div>
+    </>
+  )
+}
--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,2 +1,11 @@
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 export const imageUrl = process.env.NEXT_PUBLIC_IMG_URL;
+export const checkEnvironment = () => {
+    let base_url =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : process.env.NEXT_PUBLIC_API_URL;
+  
+    return base_url;
+  };
+// export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+// export const imageUrl = process.env.NEXT_PUBLIC_IMG_URL;
