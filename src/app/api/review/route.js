e04032af7a0163e991267b1973e82e039aec4c21
kemalmao19 <sebelumtidur14@gmail.com>
2023-12-31 23:58:44 +0700

    fix: api add story& review

--- src/app/api/review/route.js
+++ src/app/api/review/route.js
@@ -1,7 +1,6 @@
 import prisma from "@/utils/prisma"
 import { NextResponse } from "next/server";
-export async function GET(req) {
-    // const {content, potential, userid}=await req.json()
+export async function GET() {
     try {
         const review= await prisma.review.findMany() 
         return NextResponse.json({review},{status:200})
@@ -10,3 +9,25 @@ export async function GET(req) {
         return NextResponse.json({errorMessage:"Error!"},{status:500})
     }
 }
+
+export async function POST(req) {
+    const data = await req.json();
+  
+    try {
+      // Create story to database
+      const review = await prisma.story.create({
+        data
+      });
+  
+      return NextResponse.json(
+        { data: review, message: "Success! Potential Review Added" },
+        { status: 201 }
+      );
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+        { errorMessage: "Something went wrong. Please try again later" },
+        { status: 500 }
+      );
+    }
+  }
