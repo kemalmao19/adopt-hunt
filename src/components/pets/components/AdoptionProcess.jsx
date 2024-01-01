6e2224c79ea7b6c08055c91e46316f4ef4206663
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-01 16:10:35 +0700

    feat: improve pet owner and potential adopter section

--- src/components/pets/components/AdoptionProcess.jsx
+++ src/components/pets/components/AdoptionProcess.jsx
@@ -81,6 +81,8 @@ export const AdoptionProcess = ({ pet, user, adopters }) => {
   };
 
   const potentialAdopter = filterDataByPetId(adopters);
+  const potentialAdopterLenght = potentialAdopter.length;
+  const isPotentialAdopter = potentialAdopterLenght > 0;
 
   // console.log(potentialAdopter)
 
@@ -107,17 +109,27 @@ export const AdoptionProcess = ({ pet, user, adopters }) => {
 
       {/* POTENTIAL ADOPTER */}
       <div className="p-5 rounded-2xl border text-center">
-        <h3 className="text-center">Potential Adopter</h3>
-        <div className="space-x-2 space-y-2">
-          {potentialAdopter.map(({ id, name }) => {
-            return (
-              <Chip key={id} color="warning" variant="flat"
-              startContent={<UserRoundCheck size={18} />}>
-                {name}
-              </Chip>
-            );
-          })}
-        </div>
+        <h3 className="text-center mb-2">
+          Potential Adopter
+        </h3>
+        {isPotentialAdopter ? (
+          <div className="space-x-2 space-y-2">
+            {potentialAdopter.map(({ id, name }) => {
+              return (
+                <Chip
+                  key={id}
+                  color="warning"
+                  variant="flat"
+                  startContent={<UserRoundCheck size={18} />}
+                >
+                  {name}
+                </Chip>
+              );
+            })}
+          </div>
+        ) : (
+          (<span className="text-sm text-gray-400">Currently no one has been interested in {pet.name} 🙁</span>)
+        )}
       </div>
 
       {/* ADOPTER FORM */}
--- src/components/pets/components/PetOwner.jsx
+++ src/components/pets/components/PetOwner.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import {
+  Card,
+  CardHeader,
+  Avatar,
+  Chip,
+} from "@nextui-org/react";
+import { MapPin } from "lucide-react";
 
 export const PetOwner = ({ user }) => {
   return (
     <div className="pet-owner p-5 rounded-2xl border">
-      <h3 className="text-center">Pet Owner</h3>
-      <p>{user.username}</p>
-      <p>{user.domicile}</p>
-      <p>{user.bio}</p>
+      <h3>Pet Owner info</h3>
+      <Card className="max-w-[340px] shadow-none">
+        <CardHeader className="justify-between pl-1 pr-0 pb-0">
+          <div className="flex gap-5">
+            <Avatar
+              isBordered
+              radius="full"
+              size="md"
+              className="w-[40px] h-[40px] uppercase"
+              name={user.username}
+            />
+            <div className="flex flex-col gap-1 items-start justify-center w-[calc(100%-60px)]">
+              <h4 className="text-small font-semibold leading-none text-default-600 capitalize">
+                {user.username}
+              </h4>
+              <h5 className="text-small tracking-tight text-default-400">
+                <p>{user.bio}</p>
+
+                <Chip
+                  startContent={<MapPin size={18} />}
+                  variant="flat"
+                  color="secondary"
+                  className="mt-2 capitalize"
+                  size="sm"
+                >
+                  {user.domicile}
+                </Chip>
+              </h5>
+            </div>
+          </div>
+        </CardHeader>
+      </Card>
     </div>
   );
 };
