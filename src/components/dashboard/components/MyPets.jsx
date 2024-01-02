26fe7f9a0a7755c9eb8c066794410474b3bde02d
kemalmao19 <sebelumtidur14@gmail.com>
2024-01-02 21:08:30 +0700

    fix: my pets title

--- src/components/dashboard/components/MyPets.jsx
+++ src/components/dashboard/components/MyPets.jsx
@@ -24,70 +24,78 @@ async function getAdopter() {
   return data;
 }
 
-export const MyPets = async({ pets }) => {
+export const MyPets = async ({ pets }) => {
   const { adopters } = await getAdopter();
 
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 my-20">
-      {pets.map((pet, index) => {
-        let imageSize = "tr:w-300,h-200";
-        let image = `${imageUrl}/${imageSize}/pets/${pet.id}/${pet.images[0]}`;
-        const petId = pet.id;
+    <>
+      <h2 id="pets" className="my-5">
+        <span className="text-ungu">My</span> Pets
+      </h2>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 my-20">
+        {pets.map((pet, index) => {
+          let imageSize = "tr:w-300,h-200";
+          let image = `${imageUrl}/${imageSize}/pets/${pet.id}/${pet.images[0]}`;
+          const petId = pet.id;
 
-        const filterDataByPetId = (adopters) => {
-          return adopters.filter((item) => item.petId === petId);
-        };
-        const potentialAdopter = filterDataByPetId(adopters);
+          const filterDataByPetId = (adopters) => {
+            return adopters.filter((item) => item.petId === petId);
+          };
+          const potentialAdopter = filterDataByPetId(adopters);
 
-        return (
-          <div key={index} className="bg-white rounded-3xl border shadow-lg hover:scale-105 transition-all ease-in duration-100 cursor-pointer">
-            <img
-              src={image}
-              alt="pet"
-              className="w-full h-auto rounded-t-3xl"
-              width={300}
-              height={200}
-            />
-            <section className="p-7 flex flex-col gap-8">
-              <div className="space-y-1">
-                <h2 className="text-3xl">{pet.name}</h2>
-                <p className="text-gray-500 thin-text">{pet.description}</p>
-              </div>
-              {pet.isAdopted ? (
-                <div className="thin-text text-orange-500">Adopted</div>
-              ) : (
-                <p className="text-gray-500 thin-text">
-                  Potential Adopter:{` ${potentialAdopter.length}`}
-                  <span className="font-bold">{pet.potentialAdopter}</span>
-                </p>
-              )}
-            </section>
-            <section className="flex justify-between items-center px-7 py-4 border-t-2 border-gray-200">
-              <div className="flex space-x-4">
-                <Link href={`/dashboard/pets/update/${pet.id}`}>
-                  <Pencil
+          return (
+            <div
+              key={index}
+              className="bg-white rounded-3xl border shadow-lg hover:scale-105 transition-all ease-in duration-100 cursor-pointer"
+            >
+              <img
+                src={image}
+                alt="pet"
+                className="w-full h-auto rounded-t-3xl"
+                width={300}
+                height={200}
+              />
+              <section className="p-7 flex flex-col gap-8">
+                <div className="space-y-1">
+                  <h2 className="text-3xl">{pet.name}</h2>
+                  <p className="text-gray-500 thin-text">{pet.description}</p>
+                </div>
+                {pet.isAdopted ? (
+                  <div className="thin-text text-orange-500">Adopted</div>
+                ) : (
+                  <p className="text-gray-500 thin-text">
+                    Potential Adopter:{` ${potentialAdopter.length}`}
+                    <span className="font-bold">{pet.potentialAdopter}</span>
+                  </p>
+                )}
+              </section>
+              <section className="flex justify-between items-center px-7 py-4 border-t-2 border-gray-200">
+                <div className="flex space-x-4">
+                  <Link href={`/dashboard/pets/update/${pet.id}`}>
+                    <Pencil
+                      strokeWidth={3}
+                      className="cursor-pointer hover:scale-110"
+                      color="green"
+                    />
+                  </Link>
+                  <Trash2
                     strokeWidth={3}
                     className="cursor-pointer hover:scale-110"
-                    color="green"
+                    color="red"
+                    onClick={() => deletePet(pet.id)}
+                  />
+                </div>
+                <Link href={`/dashboard/pets/${pet.id}`}>
+                  <Eye
+                    strokeWidth={3}
+                    className="cursor-pointer hover:scale-110"
                   />
                 </Link>
-                <Trash2
-                  strokeWidth={3}
-                  className="cursor-pointer hover:scale-110"
-                  color="red"
-                  onClick={() => deletePet(pet.id)}
-                />
-              </div>
-              <Link href={`/dashboard/pets/${pet.id}`}>
-                <Eye
-                  strokeWidth={3}
-                  className="cursor-pointer hover:scale-110"
-                />
-              </Link>
-            </section>
-          </div>
-        );
-      })}
-    </div>
+              </section>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
