3cab02b3313994a111a54bd87dcfcf23dd6970c2
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-03 00:29:29 +0700

    tweak layout

--- src/app/dashboard/pets/[id]/page.js
+++ src/app/dashboard/pets/[id]/page.js
@@ -57,9 +57,9 @@ export default async function Page({ params }) {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 mt-5">
         {/* LEFT */}
-        <div className="space-y-6 md:col-span-1 lg:col-span-2">
+        <div className="md:col-span-1 lg:col-span-2">
           <PetInfo pet={pet} />
           <PetAbout pet={pet} />
         </div>
