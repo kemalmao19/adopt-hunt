86c7ac5665433c7b20a451736f41dd012ed8ae4e
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-03 18:40:48 +0700

    improve: stroy ui

--- src/components/landing/Story.jsx
+++ src/components/landing/Story.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { checkEnvironment } from "@/config/apiUrl";
+import { Chip } from "@nextui-org/react";
+import { UserRoundCheck, PawPrint } from "lucide-react";
 
 const getAdopterName = async (x) => {
   const res = await fetch(`${checkEnvironment()}/api/adopter/${x}`);
   const adopter = await res.json();
 
   return adopter;
+};
+
+const getPetName = async (id) => {
+  const res = await fetch(`${checkEnvironment()}/api/pets/${id}`);
+  const pets = await res.json();
+
+  return pets;
 };
 
 export const Story = ({ stories }) => {
@@ -18,18 +27,31 @@ export const Story = ({ stories }) => {
           <h2>
             <span className="text-oren">Adopter</span> Stories
           </h2>
-          <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 my-20">
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
             {stories.map(async (story, index) => {
               const adopterName = await getAdopterName(story.adopterId);
-              // console.log(adopterName);
+              const petName = await getPetName(story.petId);
+              // console.log(petName);
               return (
-                <div key={index}>
-                  <section className="text-gray-500">{`"${story.content}"`}</section>
-                  <section className="text-gray-500">{story.rating}</section>
-                  <section className="text-gray-500">
-                    {adopterName?.adopter.name}
-                  </section>
-                </div>
+                <>
+                  <div
+                    key={index}
+                    className="mt-10 p-5 rounded-2xl border bg-white"
+                  >
+                    <h3 className="capitalize"><PawPrint className="inline-block" /> {petName.pet.name}</h3>
+                    <p className="my-5 block text-gray-400">
+                      "{story.content}"
+                    </p>
+                    <Chip
+                      color="warning"
+                      variant="flat"
+                      startContent={<UserRoundCheck size={18} />}
+                      className="bg-purple-100 text-black"
+                    >
+                      {adopterName?.adopter.name}
+                    </Chip>
+                  </div>
+                </>
               );
             })}
           </div>
--- src/components/pets/components/AdopterStory.jsx
+++ src/components/pets/components/AdopterStory.jsx
@@ -10,7 +10,7 @@ export const AdopterStory = ({ storyAdopter, adopter }) => {
       {isStory ? (
         <div className="mt-10 p-5 rounded-2xl border bg-white">
           <h3>Adopter story</h3>
-          <quote className="my-5 block text-gray-400">"{storyAdopter[0]?.content}"</quote>
+          <p className="my-5 block text-gray-400">"{storyAdopter[0]?.content}"</p>
           <Chip
             color="warning"
             variant="flat"
