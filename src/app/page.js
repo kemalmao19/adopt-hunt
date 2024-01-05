ac7dc576cb7283f372aa1415f5baf3ff580caa96
kemalmao19 <sebelumtidur14@gmail.com>
2024-01-05 17:59:24 +0700

    ngetes performa kalo ga fetch ke server sendiri 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,6 @@
 import { PetsAvailable } from "@/components/landing/PetsAvailable";
 import { Story } from "@/components/landing/Story";
-import { checkEnvironment } from "@/config/apiUrl";
 import prisma from "@/utils/prisma";
-
-
-// async function fetchData(endpoint) {
-//   const res = await fetch(`${checkEnvironment()}/api/${endpoint}`, {
-//     cache: "no-cache",
-//   });
-//   const data = await res.json();
-//   return data;
-// }
-
-// async function getPets() {
-//   const { pets } = await fetchData('pets');
-//   return pets;
-// }
 
 async function getPets() {
   const pets = await prisma.pet.findMany({
@@ -32,7 +17,7 @@ async function getPets() {
 }
 
 
-async function dapetinStory() {
+async function getStories() {
   const story = await prisma.story.findMany({
     include: {
       pets: {
@@ -53,8 +38,7 @@ async function dapetinStory() {
 
 export default async function Home() {
   const pets = await getPets();
-  // const stories = await getStories();
-  const stories = await dapetinStory();
+  const stories = await getStories();
 
   return (
     <>
@@ -63,4 +47,3 @@ export default async function Home() {
     </>
   );
 }
-export const dynamic = "force-dynamic"
